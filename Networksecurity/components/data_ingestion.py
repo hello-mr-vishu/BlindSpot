a87fe4c45c543a98a276fa1359078ab259c9807e
hello-mr-vishu <vishnurongali21@gmail.com>
2025-05-30 17:19:36 +0530

    Data Retrieve from MongoDB

---
+++ Networksecurity/components/data_ingestion.py
@@ -0,0 +1,52 @@
+from NetworkSecurity.exception.exception import NetworkSecurityException
+from NetworkSecurity.logging.logger import logging
+
+
+# configuaration of the data Ingestion Config
+from NetworkSecurity.entity.config_entity import DataIngestionConfig
+import os 
+import sys
+import pandas as pd
+import numpy as np
+import pymongo
+from typing import List
+from sklearn.model_selection import train_test_split
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URL= os.getenv("MONGO_DB_URL")
+
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig):
+        try:
+            self.data_ingestion_config=data_ingestion_config
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+    
+    def export_collection_as_dataframe(self):
+        """
+        Read data from MongoDB
+        """
+        try:
+            database_name = self.data_ingestion_config.database_name
+            collection_name=self.data_ingestion_config.collection_name
+            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            collection = self.mongo_client[database_name][collection_name]
+
+            df =pd.DataFrame(list(collection.find()))
+
+            if "_id" in df.columns.to_list():
+                df = df.drop(columns=["_id"],axis=1)
+            df.replace({"na":np.nan},inplace= True)
+            return df
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+
+    def initiate_data_ingestion(self):
+        try:
+            dataframe=self.export_collection_as_dataframe()
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
