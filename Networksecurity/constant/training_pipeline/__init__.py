8a0e201bc7ee3a4b32caa629af71c2707f4b22fe
hello-mr-vishu <vishnurongali21@gmail.com>
2025-06-03 12:46:10 +0530

    Project structure

--- Networksecurity/constant/training_pipeline/__init__.py
+++ Networksecurity/constant/training_pipeline/__init__.py
@@ -34,3 +34,7 @@ DATA_VALIDATION_VALID_DIR:str="validated"
 DATA_VALIDATION_INVALID_DIR:str="invaid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
